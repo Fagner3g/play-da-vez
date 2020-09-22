2d5eabd4f05f10cbd9ab12870ec34655a08d5775
Fagner Egidio Gomes <fagner.gomes@compasso.com.br>
2020-09-22 11:17:51 -0300

    feat: 🎸 Implementação de fontes no projeto
    Implementamos as fontes no projeto android e ios onde também criamos o
    tutorial

--- src/components/Text/styles.ts
+++ src/components/Text/styles.ts
@@ -3,4 +3,5 @@ import styled from 'styled-components/native';
 export const TextBase: any = styled.Text<{ color?: string; size?: number }>`
   color: ${({ theme, color }) => (color ? color : theme.colors.buttonText)};
   font-size: ${({ size }) => (size ? size : '16px')};
+  font-family: ${({ theme }) => theme.fonts.QuicksandBold};
 `;
--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,16 +1,23 @@
 import React from 'react';
+import { useNavigation } from '@react-navigation/native';
 
 import Logo from '~/assets/images/Logo.png';
 
 import { Container, Image, Button, ButtonArea } from './styles';
 
 const SignIn: React.FC = () => {
+  const navigation = useNavigation();
+
+  function handleSignIn() {
+    navigation.reset({ routes: [{ name: 'Home' }] });
+  }
+
   return (
     <Container>
       <Image source={Logo} />
       <ButtonArea>
         <Button onPress={() => {}} title="Google+" color="#db3236" />
-        <Button onPress={() => {}} title="Facebook" color="#3b5998" />
+        <Button onPress={() => {}} title="facebook" color="#3b5998" />
       </ButtonArea>
     </Container>
   );
